a73dad66f787fb8e3bad586470f76652d43dac01
Pedro Bastos <henriquepedro.2001@gmail.com>
2025-03-19 16:17:00 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -16,10 +16,6 @@ public class Seller  implements Serializable {
 
     private Department department;
 
-    public Seller() {
-
-    }
-
     public Seller(Integer id, String name, String email, Date birthDate, double baseSalary, Department department) {
         this.id = id;
         this.name = name;
